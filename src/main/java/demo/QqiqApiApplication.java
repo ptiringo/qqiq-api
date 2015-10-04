10dcfc97fa495d4dd4359372cf0cbe4e3301ae08
ptiringo <ptiringo@gmail.com>
2015-10-04 10:02:56 +0900

    Initial Commit

---
+++ src/main/java/demo/QqiqApiApplication.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QqiqApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QqiqApiApplication.class, args);
+    }
+}
