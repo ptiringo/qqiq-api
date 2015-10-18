88c519a3ef9295b58ebab5d5d806738ce8ae0ec4
ptiringo <ptiringo@gmail.com>
2015-10-18 23:56:21 +0900

    Add PostgreSQL DataSource settings

--- src/main/java/jp/co/qqiq/Application.java
+++ src/main/java/jp/co/qqiq/Application.java
@@ -1,13 +1,48 @@
 package jp.co.qqiq;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
+  @Autowired
+  DataSourceProperties properties;
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+  }
+
+  @Bean(destroyMethod = "close")
+  DataSource dataSource() throws URISyntaxException {
+    String url;
+    String username;
+    String password;
+
+    String databaseUrl = System.getenv("DATABASE_URL");
+    if (databaseUrl != null) {
+      URI dbUri = new URI(databaseUrl);
+      url = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath() + ":" + dbUri.getPort() + dbUri.getPath();
+      username = dbUri.getUserInfo().split(":")[0];
+      password = dbUri.getUserInfo().split(":")[1];
+    } else {
+      url = this.properties.getUrl();
+      username = this.properties.getUsername();
+      password = this.properties.getPassword();
+    }
+
+    DataSourceBuilder factory = DataSourceBuilder.create(this.properties.getClassLoader()).url(url).username(username)
+        .password(password);
+    return factory.build();
   }
 
 }
